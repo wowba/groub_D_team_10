4ce47ff62a0dde63a2b9e16f109336ac2fad6ae5
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-01-10 23:35:24 +0900

    first

--- app.py
+++ app.py
@@ -1,12 +1,70 @@
-from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+import secrets
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = secrets.token_hex(16)
+
+client = MongoClient('localhost', 27017)
+db = client.logintest
 
 
 ## HTML을 주는 부분
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]}, {'_id': False})
+        return render_template('index.html', user_info=user_info)
+    except:
+        return render_template('index.html')
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디 / 비밀번호가 일치하지 않거나 정보가 없습니다'})
+
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'GET':
+        return render_template('sign-up.html')
+    else:
+        username_receive = request.form['username_give']
+        password_receive = request.form['password_give']
+        password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+        doc = {
+            "username": username_receive,
+            "password": password_hash,
+        }
+        db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

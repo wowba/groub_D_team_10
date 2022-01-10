8d011d71abf98c5a704cbb90900745a54f25a13b
wowba <87873821+wowba@users.noreply.github.com>
2022-01-10 22:02:22 +0900

    Update ibacrawl.py

--- ibacrawl.py
+++ ibacrawl.py
@@ -12,16 +12,20 @@ client = MongoClient("localhost", 27017)
 db = client.team10
 
 # 드라이버 실행
-driver = webdriver.Chrome()
-driver.set_window_position(0, 0)
-driver.set_window_size(1920, 1080)
+
 # IBA 칵테일 사이트 들어가기
 
 links = ["https://iba-world.com/category/iba-cocktails/the-unforgettables/",
-         "https://iba-world.com/category/iba-cocktails/contemporary-classics/",
-         "https://iba-world.com/category/iba-cocktails/new-era-drinks/"]
+          "https://iba-world.com/category/iba-cocktails/contemporary-classics/",
+          "https://iba-world.com/category/iba-cocktails/new-era-drinks/"]
+
+
 
 for link in links:
+    # 드라이버 실행
+    driver = webdriver.Chrome()
+    driver.set_window_position(0, 0)
+    driver.set_window_size(1920, 1080)
     driver.get(link)
     driver.implicitly_wait(5)
 
@@ -37,72 +41,21 @@ for link in links:
         driver.set_window_size(1920, 1080)
         driver.get(link)
         driver.implicitly_wait(5)
-        try:
-            name = driver.find_element_by_class_name("entry-title").text
-            cocktail_class = driver.find_element_by_class_name(
-                "et_pb_title_meta_container").text.replace("IBA COCKTAIL,","")
-            try:
-                ingredients = driver.find_element_by_css_selector(
-                "#main-content > div > div > div > div > div.et_pb_column.et_pb_column_1_2.et_pb_column_1_tb_body.et_pb_css_mix_blend_mode_passthrough.et-last-child > div.et_pb_module.et_pb_post_content.et_pb_post_content_0_tb_body.blog-post-content > p:nth-child(2)").text
-            except:
-                ingredients = driver.find_element_by_class_name("column").text
-            method = driver.find_element_by_css_selector(
-                "#main-content > div > div > div > div > div.et_pb_column.et_pb_column_1_2.et_pb_column_1_tb_body.et_pb_css_mix_blend_mode_passthrough.et-last-child > div.et_pb_module.et_pb_post_content.et_pb_post_content_0_tb_body.blog-post-content > p:nth-child(4)").text
-            try:
-                garnish = driver.find_element_by_css_selector(
-                    "#main-content > div > div > div > div > div.et_pb_column.et_pb_column_1_2.et_pb_column_1_tb_body.et_pb_css_mix_blend_mode_passthrough.et-last-child > div.et_pb_module.et_pb_post_content.et_pb_post_content_0_tb_body.blog-post-content > p:nth-child(6)").text
-            except:
-                garnish = driver.find_element_by_css_selector(
-                    "#main-content > div > div > div > div > div.et_pb_column.et_pb_column_1_2.et_pb_column_1_tb_body.et_pb_css_mix_blend_mode_passthrough.et-last-child > div.et_pb_module.et_pb_post_content.et_pb_post_content_0_tb_body.blog-post-content > p:nth-child(7)")
-            imgsrc = driver.find_element_by_xpath("//meta[@property='og:image']").get_attribute("content")
 
-            doc = {
-                "name": name,
-                "class": cocktail_class,
-                "ingredients": ingredients,
-                "method": method,
-                "garnish": garnish,
-                "imgsrc": imgsrc,
-                "like": 0,
-                "review": [],
-                "stars": [],
-            }
-            print(name)
-            driver.close()
-            time.sleep(3)
-        except:
-            name = driver.find_element_by_class_name("entry-title").text
-            cocktail_class = driver.find_element_by_class_name(
-                "et_pb_title_meta_container").text.replace("IBA COCKTAIL,", "")
-            try:
-                ingredients = driver.find_element_by_css_selector(
-                    "#main-content > div > div > div > div > div.et_pb_column.et_pb_column_1_2.et_pb_column_1_tb_body.et_pb_css_mix_blend_mode_passthrough.et-last-child > div.et_pb_module.et_pb_post_content.et_pb_post_content_0_tb_body.blog-post-content > p:nth-child(3)").text
-            except:
-                ingredients = driver.find_element_by_class_name("column").text
-            method = driver.find_element_by_css_selector(
-                "#main-content > div > div > div > div > div.et_pb_column.et_pb_column_1_2.et_pb_column_1_tb_body.et_pb_css_mix_blend_mode_passthrough.et-last-child > div.et_pb_module.et_pb_post_content.et_pb_post_content_0_tb_body.blog-post-content > p:nth-child(5)").text
-            try:
-                garnish = driver.find_element_by_css_selector(
-                    "#main-content > div > div > div > div > div.et_pb_column.et_pb_column_1_2.et_pb_column_1_tb_body.et_pb_css_mix_blend_mode_passthrough.et-last-child > div.et_pb_module.et_pb_post_content.et_pb_post_content_0_tb_body.blog-post-content > p:nth-child(7)").text
-            except:
-                garnish = driver.find_element_by_css_selector(
-                    "#main-content > div > div > div > div > div.et_pb_column.et_pb_column_1_2.et_pb_column_1_tb_body.et_pb_css_mix_blend_mode_passthrough.et-last-child > div.et_pb_module.et_pb_post_content.et_pb_post_content_0_tb_body.blog-post-content > p:nth-child(8)")
-            imgsrc = driver.find_element_by_xpath("//meta[@property='og:image']").get_attribute("content")
-
-            doc = {
-                "name": name,
-                "class": cocktail_class,
-                "ingredients": ingredients,
-                "method": method,
-                "garnish": garnish,
-                "imgsrc": imgsrc,
-                "like": 0,
-                "review": [],
-                "stars": [],
-            }
-            print(name)
-            driver.close()
-            time.sleep(3)
+        name = driver.find_element_by_class_name("entry-title").text
+        cocktail_class = driver.find_element_by_class_name(
+            "et_pb_title_meta_container").text.replace("IBA COCKTAIL,", "")
+        desc = driver.find_element_by_xpath("//meta[@property='og:description']").get_attribute("content").split("METHOD")
+        ingredient = desc[0]
+        desc2 = desc[1].split("GARNISH")
+        method = desc2[0]
+        imgsrc = driver.find_element_by_xpath("//meta[@property='og:image']").get_attribute("content")
+        print(name)
+        print(ingredient)
+        print(method)
+        print(imgsrc)
+        driver.close()
+        time.sleep(3)
 
     print(link, "크롤링 끝")
 

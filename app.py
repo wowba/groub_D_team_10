9df12d564a5261265da573bd4628d74535523bdd
wowba <87873821+wowba@users.noreply.github.com>
2022-01-12 00:47:48 +0900

    add like function! Yeah!

--- app.py
+++ app.py
@@ -70,6 +70,7 @@ def sign_up():
         doc = {
             "username": username_receive,
             "password": password_hash,
+            "like_list": [],
         }
         db.users.insert_one(doc)
     return jsonify({'result': 'success'})
@@ -151,7 +152,7 @@ def to_write_page():
             "like": 0,
             "review": [],
             "imgsrc": imgsrc_receive,
-            "stars": []
+            "stars": [],
         }
 
         db.cocktails.insert_one(doc)
@@ -164,13 +165,62 @@ def to_write_page():
 def random_list():
     random_list = list(db.cocktails.find({}, {'_id': False}))
     random.shuffle(random_list)
-    return jsonify({'result': random_list})
+    try:
+        token_receive = request.cookies.get('mytoken')
+    except:
+        pass
+
+    user_like_list = ''
+    if token_receive is not None:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        user_like_list = user_info["like_list"]
+
+    return jsonify({'result': random_list,"user_like_list":user_like_list})
 
 # TODO 좋아요 순 칵테일 추천 API
 @app.route('/api/likerecommend', methods=['GET'])
 def like_list():
     like_list = list(db.cocktails.find({}, {'_id': False}).sort('like', -1))
-    return jsonify({'result': like_list})
+    try:
+        token_receive = request.cookies.get('mytoken')
+    except:
+        pass
+
+    user_like_list = ''
+    if token_receive is not None:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        user_like_list = user_info["like_list"]
+
+    return jsonify({'result': like_list, "user_like_list": user_like_list})
+
+
+# TODO 좋아요 버튼 기능 추가
+@app.route('/api/likeclick', methods=["POST"])
+def like_click():
+    name_receive = request.form["name_give"]
+    token_receive = request.cookies.get('mytoken')
+
+    if token_receive is not None:
+
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        user_like_list = user_info["like_list"]
+
+        if name_receive in user_like_list:
+            db.users.update_one({"username":payload["id"]},{'$pull': {'like_list': name_receive}})
+            db.cocktails.update_one({"name":name_receive},{'$inc': {'like': -1}})
+            print("좋아요 취소")
+            return jsonify({'msg': '좋아요 취소'})
+        elif name_receive not in user_like_list:
+            db.users.update_one({"username": payload["id"]}, {'$push': {'like_list': name_receive}})
+            db.cocktails.update_one({"name": name_receive}, {'$inc': {'like': 1}})
+            print("좋아요")
+            return jsonify({'msg': '좋아요!'})
+    else:
+        print("로그인 먼저")
+        return jsonify({'error': '로그인이 필요합니다. 로그인 페이지로 이동합니다.'})
 
 
 if __name__ == '__main__':
--- static/js/likecocktail.js
+++ static/js/likecocktail.js
@@ -5,6 +5,7 @@ function likeRecommend() {
         data: {},
         success: function (response) {
             const likeList = response['result']
+            const userLikeList = response['user_like_list'];
 
             for (let i = 0; i < 12; i++) {
                 const name = likeList[i]['name']
@@ -12,21 +13,73 @@ function likeRecommend() {
                 const likeCount = likeList[i]['like']
                 const imgsrc = likeList[i]['imgsrc']
 
-                let temp_html =`    <div class="item">
+                let temp_html = ""
+
+                if (userLikeList.includes(name)){
+                    temp_html =`    <div class="item">
                                         <img class="card-img-top" src="${imgsrc}" alt="Card image cap">
                                         <div class="item-wrapper">
                                             <a href="/api/view?cocktailname=${name}"><div class="title">${name}</div></a>
                                             <div class="address">Recipe : ${cocktail_class}</div>
                                             <div onclick="handleClickLike('${name}')" class="click">
                                                 <img src="/static/img/icon/suit-heart-fill.svg" class="${name} active" style="width: 20px; height: 20px"/>
-                                                 <span style="margin-left: 5px">${likeCount}</span>
+                                                <span class="${name}" style="margin-left: 5px">${likeCount}</span>
                                             </div>
                                         </div>
                                     </div>`
 
+                } else {
+                    temp_html =`    <div class="item">
+                                        <img class="card-img-top" src="${imgsrc}" alt="Card image cap">
+                                        <div class="item-wrapper">
+                                            <a href="/api/view?cocktailname=${name}"><div class="title">${name}</div></a>
+                                            <div class="address">Recipe : ${cocktail_class}</div>
+                                            <div onclick="handleClickLike('${name}')" class="click">
+                                                <img src="/static/img/icon/suit-heart.svg" class="${name} active" style="width: 20px; height: 20px"/>
+                                                <span class="${name}" style="margin-left: 5px">${likeCount}</span>
+                                            </div>
+                                        </div>
+                                    </div>`
+                }
                 $("#main-like-list").append(temp_html)
 
             }
         }
     })
+}
+
+function handleClickLike(name) {
+    $.ajax({
+        type: "POST",
+        url: "/api/likeclick",
+        data: {name_give: name},
+        success: function (response){
+            changeLikeStatus(name)
+        }
+    })
+}
+
+function changeLikeStatus(name) {
+    const elements = document.getElementsByClassName(`${name}`);
+
+    const countElement = elements.item(1);  // 카운트 element
+    const iconElement = elements.item(0);  // 좋아요 아이콘 element
+
+    const currentCount = countElement.innerText;
+    const num = Number(currentCount);
+
+    // const currentIcon = iconElement.classList.contains('active');
+    console.log(countElement)
+    const currentIcon = iconElement.getAttribute('src') === '/static/img/icon/suit-heart.svg';
+
+
+    // console.log(currentIcon)
+
+    if (currentIcon) {
+        countElement.innerText = (num + 1).toString();
+        iconElement.setAttribute('src', '/static/img/icon/suit-heart-fill.svg');
+    } else {
+        countElement.innerText = (num - 1).toString();
+        iconElement.setAttribute('src', '/static/img/icon/suit-heart.svg');
+    }
 }

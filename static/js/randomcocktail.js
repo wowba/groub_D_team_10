0d9d88ddde1cf75f772d286715466856a630d3f8
wowba <87873821+wowba@users.noreply.github.com>
2022-01-11 16:12:14 +0900

    title click avliable

--- static/js/randomcocktail.js
+++ static/js/randomcocktail.js
@@ -6,17 +6,18 @@ function randomRecommend() {
         success: function (response) {
             const likeList = response['result']
 
-            for (let i = 0; i < 9; i++) {
+            for (let i = 0; i < 12; i++) {
                 const name = likeList[i]['name']
-                // const stars = likeList[i]['stars']
+                const cocktail_class = likeList[i]["class"]
                 const likeCount = likeList[i]['like']
                 const imgsrc = likeList[i]['imgsrc']
 
                 let temp_html =`<div class="item">
                                     <img class="card-img-top" src="${imgsrc}" alt="Card image cap">
                                         <div class="item-wrapper">
-                                            <div class="title">${name}</div>
-                                            <div class="address">좋아요: ${likeCount}</div>
+                                        
+                                               <a href="/api/view?cocktailname=${name}"><div class="title">${name}</div></a>
+                                            <div class="address">Recipe : ${cocktail_class}</div>
                                         </div>
                                     </div>
                                  </div>`

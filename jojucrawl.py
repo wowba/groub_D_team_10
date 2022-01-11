cb0ef6997a0c4b3ddbccd158c129a886664ddbcd
wowba <87873821+wowba@users.noreply.github.com>
2022-01-11 09:24:04 +0900

    joju

--- jojucrawl.py
+++ jojucrawl.py
@@ -15,3 +15,13 @@ links = ["https://www.beveragemaster.kr/index.php?mid=license_practice&page=1",
           "https://www.beveragemaster.kr/index.php?mid=license_practice&page=2",
           "https://www.beveragemaster.kr/index.php?mid=license_practice&page=3",
          "https://www.beveragemaster.kr/index.php?mid=license_practice&page=4"]
+
+for link in links:
+# 드라이버 실행
+    driver = webdriver.Chrome()
+    # 실행 옵션 설정
+    driver.set_window_position(0, 0)
+    driver.set_window_size(1920, 1080)
+    # 링크 이동
+    driver.get(link)
+    driver.implicitly_wait(5)
